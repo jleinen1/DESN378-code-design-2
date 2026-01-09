4e02572eac19390e22b1018e11cc368e70d33fce
Justine Leinen <jleinen1@ewu.edu>
2026-01-08 22:51:59 -0800

    Refactor console.log statements to template literals

--- week-0/learn-0-1-codedex-console-LJ.js
+++ week-0/learn-0-1-codedex-console-LJ.js
@@ -9,23 +9,23 @@
 // Add your custom greeting below:
 // Hello internet!
 // --------------------------------------------
-console.log("Hello internet!");
+console.log(`Hello internet!`);
 
 // --------------------------------------------
 // EXERCISE 2: THE CONSOLE
 // Fav food: Choc Chip cookies
 // --------------------------------------------
-console.log("Choc chip cookies");
+console.log(`Choc chip cookies`);
 
 // --------------------------------------------
 // EXERCISE 3: LETTER TREE
 // The pattern: a, b c, d e f, g h i j, k
 // --------------------------------------------
-console.log("   a   ");
-console.log("  b c");
-console.log(" d e f");
-console.log("g h i j");
-console.log("   k");
+console.log(`   a   `);
+console.log(`  b c`);
+console.log(` d e f`);
+console.log(`g h i j`);
+console.log(`   k`);
 
 // --------------------------------------------
 // EXERCISE 4: SECRET RECIPE
@@ -49,14 +49,14 @@ console.log("   k");
 // Gently mix salad dressing in a bowl.
 // Refrigerate pasta salad until chilled for best results, at least 30 minutes before serving. Enjoy!
 
-console.log("Ingredients:");
-console.log("16 oz rotini pasta");
-console.log("16 oz of Italian dressing");
-console.log("2 chopped cucumbers");
-console.log("6 tomatoes");
-console.log("a bunch of green onions");
+console.log(`Ingredients:`);
+console.log(`16 oz rotini pasta`);
+console.log(`16 oz of Italian dressing`);
+console.log(`2 chopped cucumbers`);
+console.log(`6 tomatoes`);
+console.log(`a bunch of green onions`);
 
-console.log("Instructions:");
+console.log(`Instructions:`);
 console.log(`1. Gather all ingredients.`);
 console.log(`2. Bring a large pot of lightly salted water to a boil. Place pasta in the pot, cook for 8 to 12 minutes, until al dente, and drain.`);
 console.log(`3. Toss cooked pasta with Italian dressing, cucumbers, tomatoes, and green onions in a large bowl.`);
@@ -75,15 +75,4 @@ Add some parmesan cheese for extra flavor.
 console.log(`
 ==================================
         McDONALD'S RECEIPT
-----------------------------------
-           Order Details
-----------------------------------
-Item         Quantity   Price
-----------------------------------
-McNuggets     1          4.99
-Big Mac       1          5.49
-McFlurry      1          3.29
-----------------------------------
-Total                     13.77
-==================================
-`);
+-----------
