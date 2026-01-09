4393551f3c293b447219e57692ee2d1364a6e327
Justine Leinen <jleinen1@ewu.edu>
2026-01-08 22:05:04 -0800

    Add learn 0-1 Codedex exercises

--- week-0/learn-0-1-codedex-console-LJ.js
+++ week-0/learn-0-1-codedex-console-LJ.js
@@ -1,1 +1,89 @@
+// ============================================
+// TUTORIAL 0-1: CODEDEX CONSOLE FUNDAMENTALS
+// Student: [Justine Leinen]
+// Date: [1/8/25]
+// ============================================
+​
+// --------------------------------------------
+// EXERCISE 1: SETTING UP
+// Add your custom greeting below:
+// Hello internet!
+// --------------------------------------------
+console.log("Hello internet!");​
+​
+// --------------------------------------------
+// EXERCISE 2: THE CONSOLE
+// Fav food: Choc Chip cookies
+// --------------------------------------------
+console.log("Choc chip cookies")
+​​
+// --------------------------------------------
+// EXERCISE 3: LETTER TREE
+// The pattern: a, b c, d e f, g h i j, k
+// --------------------------------------------
+console.log("   a   ");
+console.log("  b c");
+console.log(" d e f");
+console.log("g h i j");
+console.log("   k");
+​​
+// --------------------------------------------
+// EXERCISE 4: SECRET RECIPE
+// Use comments to document your recipe
+// Include a "secret ingredient" comment
+// My favorite dish: Pasta Salad
+// Justine
+// --------------------------------------------
 
+// Ingredients:
+// 16 oz rotini pasta
+// 16 oz Italian dressing
+// 2 chopped cucumbers
+// 6 tomatoes
+// a bunch of green onions
+
+// Instructions:
+// Gather all ingredients.
+// Bring a large pot of lightly salted water to a boil. Place pasta in the pot, cook for 8 to 12 minutes, until al dente, and drain.
+// Toss cooked pasta with Italian dressing, cucumbers, tomatoes, and green onions in a large bowl.
+// Gently mix salad dressing in a bowl.
+// Refrigerate pasta salad until chilled for best results, at least 30 minutes before serving. Enjoy!
+
+console.log("Ingredients:");
+console.log("16 oz rotini pasta");
+console.log("16 oz of Italian dressing");
+console.log("2 chopped cucumbers");
+console.log("6 tomatoes");
+console.log("a bunch of green onions");
+
+console.log("Instructions:");
+console.log("1. Gather all ingredients.");
+console.log("2. Bring a large pot of lightly salted water to a boil. Place pasta in the pot, cook for 8 to 12 minutes, until al dente, and drain.");
+console.log("3. Toss cooked pasta with Italian dressing, cucumbers, tomatoes, and green onions in a large bowl.");
+console.log("4. Gently mix salad and dressing in a bowl");
+console.log("5. Refrigerate pasta salad until chilled for best results, at least 30 minutes before serving. Enjoy!");
+
+/* 
+Secret Ingredient 🤫:
+Add some parmesan cheese for extra flavor.
+*/
+​​
+// --------------------------------------------
+// EXERCISE 5: RECEIPT
+// Format a receipt from a recent purchase
+// --------------------------------------------
+console.log(`
+==================================
+        McDONALD'S RECEIPT
+----------------------------------
+           Order Details
+----------------------------------
+Item         Quantity   Price
+----------------------------------
+McNuggets     1          4.99
+Big Mac       1          5.49
+McFlurry      1          3.29
+----------------------------------
+Total                     13.77
+==================================
+`);
