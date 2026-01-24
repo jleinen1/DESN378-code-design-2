cbd186ed67dc2fbaf439db882159d2e454789fa3
Justine Leinen <jleinen1@ewu.edu>
2026-01-23 17:40:24 -0800

    Add conditional greetings and Magic 8 Ball logic

--- week-2/codedex/tutorial-2-2-codedex-conditionals.js
+++ week-2/codedex/tutorial-2-2-codedex-conditionals.js
@@ -36,16 +36,32 @@ if (hour < 12) {
 // If hour < 12: morning greeting
 // Else: afternoon greeting
 // --------------------------------------------
+let hour = 12;
+
+if (hour < 10) {
+  console.log("Good morning!!");
+  console.log("Brush teeth");
+  console.log("Make coffee + breakfast");
+  console.log("Go to school or work :)");
+} else {
+  console.log("Good afternoon!");
+}​
 ​
-​
-​
-​
+​​
 // --------------------------------------------
 // EXERCISE 14: pH LEVELS
 // Check if pH is basic, acidic, or neutral
 // Use else if for multiple conditions
 // --------------------------------------------
-​
+const ph = 7;
+
+if (ph > 7) {
+  console.log("basic");
+} else if (ph < 7) {
+  console.log("acidic");
+} else {
+  console.log("neutral");
+}​
 ​
 ​
 ​
@@ -55,8 +71,36 @@ if (hour < 12) {
 // Return different responses based on number
 // Format: Question / Magic 8 Ball Answer
 // --------------------------------------------
-​
-​
+​const question = "will it be sunny today";
+
+const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+let answer = "";
+
+if (randomNumber === 2){
+  answer = 'yes definitely';
+} else if (randomNumber === 6) {
+  answer = 'it is decidedly so';
+} else if (randomNumber === 1) {
+  answer = 'without a doubt';
+} else if (randomNumber === 3) {
+  answer = 'reply hazy, try again';
+} else if (randomNumber === 7) {
+  answer = 'ask again later';
+} else if (randomNumber === 8) {
+  answer = 'better not tell you now';
+} else if (randomNumber === 2) {
+  answer = 'my sources say no';
+} else if (randomNumber === 3) {
+  answer = 'outlook not so good';
+} else if (randomNumber === 6) {
+  answer = 'very doubtful';
+} else {
+  answer = 'error';
+}
+
+console.log("Question: ", question);
+console.log("Answer:   ", answer);​
 ​
 ​
 // --------------------------------------------
