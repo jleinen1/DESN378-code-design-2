804977a0ad9c260909ccbe26293182d564ef46cf
Justine Leinen <jleinen1@ewu.edu>
2026-01-23 16:43:12 -0800

    Add exercises for conditionals in tutorial

--- week-2/codedex/tutorial-2-2-codedex-conditionals.js
+++ week-2/codedex/tutorial-2-2-codedex-conditionals.js
@@ -1,1 +1,76 @@
+// ============================================
+// TUTORIAL 2-2: CODEDEX CONDITIONALS
+// Student: [Your Name]
+// Date: [Date]
+// ============================================
+​
+// --------------------------------------------
+// EXERCISE 11: COIN FLIP
+// Simulate a coin toss using Math.random()
+// Output "Heads" or "Tails"
+// --------------------------------------------
+let num = Math.random();
 
+if (num > 0.5) {
+  console.log("Heads");
+} else {
+  console.log("Tails");
+}​
+​​
+// --------------------------------------------
+// EXERCISE 12: GOOD MORNING
+// Check if hour < 12
+// If true, print morning greeting with routines
+// --------------------------------------------
+​let hour = 10
+if (hour < 12) {
+  console.log("good morning!!")
+  console.log("brush teeth")
+  console.log("make coffee + breakfast")
+  console.log("go to school or work :)")
+}​
+​​
+// --------------------------------------------
+// EXERCISE 13: GOOD AFTERNOON
+// Add else clause to Exercise 12
+// If hour < 12: morning greeting
+// Else: afternoon greeting
+// --------------------------------------------
+​
+​
+​
+​
+// --------------------------------------------
+// EXERCISE 14: pH LEVELS
+// Check if pH is basic, acidic, or neutral
+// Use else if for multiple conditions
+// --------------------------------------------
+​
+​
+​
+​
+// --------------------------------------------
+// EXERCISE 15: MAGIC 8 BALL
+// Generate random number 0-8
+// Return different responses based on number
+// Format: Question / Magic 8 Ball Answer
+// --------------------------------------------
+​
+​
+​
+​
+// --------------------------------------------
+// EXERCISE 16: AIR QUALITY INDEX
+// Check AQI ranges using logical operators
+// 0-50: Good, 51-100: Moderate, etc.
+// --------------------------------------------
+​
+​
+​
+​
+// --------------------------------------------
+// EXERCISE 17: ROCK PAPER SCISSORS
+// Player picks 0, 1, or 2
+// Computer picks random 0-2
+// Determine winner using conditionals
+// --------------------------------------------
